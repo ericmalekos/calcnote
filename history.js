8f56e2d19a93dd03e727693400cf6d804233559f
Eric <esmalekos@pipeline.sbcc.edu>
2019-10-20 11:59:22 -0700

    Add files via upload

---
+++ history.js
@@ -0,0 +1,36 @@
+/*
+var move = document.getElementsByID("move");
+var i;
+for (i = 0; i < close.length; i++) {
+  move[i].onclick = function() {
+    var div = this.parentElement;
+    document.getElementById(noteText).appendChild(this.textContent);
+  }
+}*/
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement(exp, out) {
+  var li = document.createElement("li");
+  var t = document.createTextNode(exp + " = "+ out);
+  li.appendChild(t);
+  document.getElementById("histList").appendChild(li);
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u27A1");
+  span.className = "move";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  /*for (i = 0; i < close.length; i++) {
+    move[i].onclick = function() {
+      var div = this.parentElement;
+      document.getElementById(noteText).appendChild(this.textContent);
+    }
+  }*/
+}
